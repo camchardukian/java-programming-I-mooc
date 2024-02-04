f976a1945cc38ef90f77ad3b1bcb7ca6eac0b9b4
Cameron Chardukian <camchardukian@gmail.com>
2024-02-04 16:44:46 +0700

    finished through 4.22

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,13 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            int inputInt = Integer.valueOf(input);
+            System.out.println(inputInt * inputInt * inputInt);
+        }
     }
 }
