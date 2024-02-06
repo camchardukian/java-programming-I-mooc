86654cb6ee48d00408c4c7371c3713b612d46781
Cameron Chardukian <camchardukian@gmail.com>
2024-02-06 21:57:07 +0700

    finished through 4.31 (all of part 4)

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,18 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Name of the file:");
+        String fileName = scanner.nextLine();
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String[] data = fileScanner.nextLine().split(",");
+                String name = data[0];
+                int age = Integer.valueOf(data[1]);
+                String yearOrYears = age != 1 ? " years" : " year";
+                System.out.println(name + ", age: " + age + yearOrYears);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,36 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("File:");
+        String file = scan.nextLine();
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        int gamesPlayed = 0;
+        int wins = 0;
+        int losses = 0;
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.indexOf(team) > -1) {
+                    gamesPlayed++;
+                    String[] data = line.split(",");
+                    int teamIndex = data[0].equals(team) ? 0 : 1;
+                    int opponentIndex = teamIndex == 0 ? 1 : 0;
+                    int teamScore = Integer.valueOf(data[teamIndex + 2]);
+                    int opponentScore = Integer.valueOf(data[opponentIndex + 2]);
+                    if (teamScore > opponentScore) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println("Games: " + gamesPlayed);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
 
     }
 
