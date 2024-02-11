0c27b687332207c7a36596e0f36b9768c2099dbe
Cameron Chardukian <camchardukian@gmail.com>
2024-02-11 10:52:42 +0700

    finished through 5.11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,23 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+        if (this.squares < compared.squares) {
+            return false;
+        }
+        return true;
+    }
+
+    // fix this to be int in the next coding session.
+    public int priceDifference(Apartment compared) {
+        return Math.abs((this.princePerSquare * this.squares) - (compared.princePerSquare * compared.squares));
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        if (this.princePerSquare * this.squares < compared.princePerSquare * compared.squares) {
+            return false;
+        }
+        return true;
+    }
+
 }
