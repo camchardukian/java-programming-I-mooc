67d3b1f62c72536413e95798c4f0f320331d7426
Cameron Chardukian <camchardukian@gmail.com>
2024-02-23 23:27:39 +0700

    finished through 5.15

--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -6,7 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        ArrayList<Item> items = new ArrayList<>();
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String identifier = scanner.nextLine();
+            if (identifier.isEmpty()) {
+                break;
+            }
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            Item newItem = new Item(name, identifier);
+            Boolean shouldAddItem = true;
+            for (Item existingItem : items) {
+                if (existingItem.equals(newItem)) {
+                    shouldAddItem = false;
+                }
+            }
+            if (shouldAddItem) {
+                items.add(newItem);
+            }
+        }
+        System.out.println("==Items==");
+        for (Item item : items) {
+            System.out.println(item.getIdentifier() + ": " + item.getName());
+        }
     }
 }
