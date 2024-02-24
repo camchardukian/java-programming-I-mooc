94becd9fbc3f8df0a83c35b0296545a8384ed4a2
Cameron Chardukian <camchardukian@gmail.com>
2024-02-24 23:40:05 +0700

    finished through 5.16

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -26,11 +26,37 @@ public class SimpleDate {
         }
 
         if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+                this.day < compared.day) {
             return true;
         }
 
         return false;
+    }
+
+    public void advance() {
+        if (this.day + 1 > 30) {
+            this.day = 1;
+            if (this.month + 1 > 12) {
+                this.month = 1;
+                this.year = this.year + 1;
+            } else {
+                this.month = this.month + 1;
+            }
+        } else {
+            this.day = this.day + 1;
+        }
+    }
+
+    public void advance(int howManyDays) {
+        for (int i = 0; i < howManyDays; i++) {
+            advance();
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+        newDate.advance(days);
+        return newDate;
     }
 
 }
