38a588d54d9cd62358bd1bdc696ac86954031200
Cameron Chardukian <camchardukian@gmail.com>
2024-02-25 10:02:17 +0700

    finished part 5

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,35 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros > compared.euros || (this.euros == compared.euros && this.cents > compared.cents)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Money minus(Money decreaser) {
+        int tempEuros = this.euros - decreaser.euros;
+        int tempCents = this.cents - decreaser.cents;
+        System.out.println(tempEuros);
+        System.out.println(tempCents);
+
+        if (tempCents < 0) {
+            tempEuros--;
+            tempCents = 100 - Math.abs(tempCents);
+        }
+        if (tempEuros < 0) {
+            tempEuros = 0;
+            tempCents = 0;
+        }
+        return new Money(tempEuros, tempCents);
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
