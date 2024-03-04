2791a1452e4b0b288c46968e3679723ea31ec0c0
Cameron Chardukian <camchardukian@gmail.com>
2024-03-05 01:37:41 +0700

    finished through 7.1

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-
-
+// I don't understand what I'm supposed to be doing for this exercise so I'm skipping it for now.
+// I write tests with Jest and RTL at my day job so I know the basics of unit testing, just the instructions for this exercise aren't clear.
 public class ExerciseManagementTest {
-
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        ExerciseManagement management = new ExerciseManagement();
+        assertEquals(0, management.exerciseList().size());
+    }
 }
--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,60 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
-            System.out.print("> ");
-
+            System.out.println("First: " + firstContainer + "/100");
+            System.out.println("Second: " + secondContainer + "/100");
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = 0;
+            if (parts.length == 2) {
+                amount = Integer.valueOf(parts[1]);
+            }
+            if (command.equals("add")) {
+                if (amount < 0) {
+                    continue;
+                }
+                int sum = firstContainer + amount;
+                if (sum > 100) {
+                    firstContainer = 100;
+                } else {
+                    firstContainer = sum;
+                }
+            } else if (command.equals("move")) {
+                if (amount <= 0 || firstContainer == 0) {
+                    return;
+                }
+                int difference = firstContainer - amount;
+                if (difference < 0) {
+                    int sum = secondContainer + firstContainer;
+                    secondContainer = sum;
+                    firstContainer = 0;
+                } else {
+                    firstContainer = difference;
+                    int sum = secondContainer + amount;
+                    if (sum <= 100) {
+                        secondContainer = sum;
+                    } else {
+                        secondContainer = 100;
+                    }
+                }
+            } else if (command.equals("remove")) {
+                if (amount <= 0) {
+                    return;
+                }
+                int difference = secondContainer - amount;
+                if (difference < 0) {
+                    secondContainer = 0;
+                } else {
+                    secondContainer = difference;
+                }
+            } else if (command.equals("quit")) {
                 break;
             }
-
         }
     }
 
